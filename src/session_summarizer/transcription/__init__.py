bc3f3b7a3f49de7b117320c91d9d71d0410f77ae
Erik Saltwell <erik.saltwell@gmail.com>
2026-03-24 14:57:10 -0700

    removed orphaned from satement in init.py for transcription

--- src/session_summarizer/transcription/__init__.py
+++ src/session_summarizer/transcription/__init__.py
@@ -1,5 +1,4 @@
 from .canary_qwen_transcriber import CanaryQwenTranscriber
 from .whisper_large_transcriber import WhisperLargeTranscriber
-from .whisperx_transcriber import WhisperXTranscriber
 
-__all__ = ["WhisperLargeTranscriber", "WhisperXTranscriber", "CanaryQwenTranscriber"]
+__all__ = ["WhisperLargeTranscriber", "CanaryQwenTranscriber"]
