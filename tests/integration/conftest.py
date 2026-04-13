ef3d7ee32409f04e0565df632a03b992a7e7b326
Erik Saltwell <erik.saltwell@gmail.com>
2026-04-13 10:31:05 -0700

    mypy

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -41,7 +41,7 @@ def transcription_result() -> TranscriptionResult:
             shutil.copy2(src, dst)
 
         # Register speakers
-        RegisterSpeakersCommand(True).execute(test_logger)
+        RegisterSpeakersCommand().execute(test_logger)
 
         # Run transcription
         TranscribeAudioCommand(session_id=SESSION_ID).execute(test_logger)
