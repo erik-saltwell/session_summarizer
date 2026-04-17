365633da34e8531717b3158feeb6b2ffcab4b190
Erik Saltwell <erik.saltwell@gmail.com>
2026-04-17 13:44:51 -0700

    additional dangling smart turn reference

--- src/session_summarizer/turn_detection/__init__.py
+++
@@ -1,8 +0,0 @@
-from .smart_turn_predictor import LocalSmartTurnPredictor, SmartTurnPredictor
-from .smart_turn_scorer import score_clips_with_smart_turn
-
-__all__ = [
-    "SmartTurnPredictor",
-    "LocalSmartTurnPredictor",
-    "score_clips_with_smart_turn",
-]
