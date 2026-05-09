5a77568eecf64821f01fe05b6bd8f8303f68e42b
Erik Saltwell <erik.saltwell@gmail.com>
2026-05-09 15:52:23 -0700

    fixed tests to reflect dead code removal

--- tests/unit/test_clean_audio_eleven_labs_import.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-
-def test_clean_audio_eleven_labs_command_imports() -> None:
-    from session_summarizer.commands.clean_audio_eleven_labs import CleanAudioElevenLabsCommand
-
-    assert CleanAudioElevenLabsCommand.__name__ == "CleanAudioElevenLabsCommand"
